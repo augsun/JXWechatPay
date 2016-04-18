5e4988741dab63fcd4b2f7a40450a2c11da38720
CoderSun <shiba_iosJX@shiba-iosjxs-mbp.local>
2016-04-18 16:45:32 +0800

    text

--- JXWechatPay/JXWechatPayManager/JXWechatPayManager.h
+++ JXWechatPay/JXWechatPayManager/JXWechatPayManager.h
@@ -6,26 +6,27 @@
 //  Copyright © 2016 shiba_iosJX. All rights reserved.
 //
 
+//  JXWechatPayManager /* https://github.com/augsun/JXWechatPay */
+
+/*
+ * 1, 导入 sdk
+ * 2, 导入 frameworks:
+ *    SystemConfiguration.framework
+ *    CoreTelephony.framework
+ *    libz.tbd
+ *    libsqlite3.0.tbd
+ *    libc++.tbd
+ * 3, Info.plist 里加入数组 LSApplicationQueriesSchemes weixin
+ * 4, URL types 里加入 对应的 appid
+ * 5, 在 AppDelegate 里注册: - (BOOL)jx_registerApp:(NSString *)appid withDescription:(NSString *)appdesc;
+ *    在 AppDelegate 里重写: - (BOOL)jx_handleOpenURL:(NSURL *)url;
+ *
+ */
+
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
 #import "WXApi.h"
-
-/*
- 1, 导入 sdk
- 2, 导入 frameworks:
-    SystemConfiguration.framework
-    CoreTelephony.framework
-    libz.tbd
-    libsqlite3.0.tbd
-    libc++.tbd
- 3, Info.plist 里加入数组 LSApplicationQueriesSchemes weixin
- 4, URL types 里加入 对应的 appid
- 5, 在 AppDelegate 里注册: - (BOOL)jx_registerApp:(NSString *)appid withDescription:(NSString *)appdesc;
-    在 AppDelegate 里重写: - (BOOL)jx_handleOpenURL:(NSURL *)url;
- 6,
- 
-*/
 
 typedef NS_ENUM(NSUInteger, JXWechatPayResult) {
     JXWechatPaySuccess,
--- README.md
+++ README.md
@@ -1,10 +1,17 @@
+
 <p align="center" >
-  <img src="https://github.com/augsun/JXWechatPay/blob/master/JXWechatPay/Assets.xcassets/AppIcon.appiconset/JXWechatPay_180.png" alt="JXRollView" title="JXRollView">
+  <img src="https://github.com/augsun/JXRollView/blob/master/JXRollView/Assets.xcassets/AppIcon.appiconset/JXRollView_180.png" alt="JXRollView" title="JXRollView">
 </p>
+
 <p align="center" >
-WeChat payment manager.
+The easiest way to create an infinite loop scroll view.
 </p>
+
 [![Twitter](https://img.shields.io/badge/twitter-@jianxingangel-blue.svg?style=flat-square)](http://twitter.com/jianxingangel)
+
+<p align="right" >
+NO BEST ONLY CLOSER.
+</p>
 
 ##Easy way, easy way, easy way
 Important events say three times!
